e9347e26517af262e4ee22c31c90f9cf6843a26d
Justin May <justin.may@puppetlabs.com>
2014-05-29 11:42:26 -0700

    Docs

--- dev/user.clj
+++ dev/user.clj
@@ -6,6 +6,9 @@
 (def test-xml (xml/parse "dev-resources/tests.xml"))
 
 (defn get-failures [abs-path]
+  "Output the relative path to all test case failures. abs-path contains
+  the absolute path (ending in a '/' to strip away from the test case
+  filenames."
   (->> test-xml
        core/failures
        core/test-case-names
